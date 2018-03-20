da1ba5f31495d03ce64b4097232cb37c7c99fb5e
Andrew Yates <andrewmacu@me.com>
2018-03-20 12:57:12 -0700

    Update category.js

--- routes/create/category.js
+++ routes/create/category.js
@@ -1,18 +1,29 @@
-const auth = require(_base + 'middleware/Authenticate');
+const auth = require(_base + 'middleware/authenticate');
+
+const Category = require(_base + 'models/category');
 
 module.exports = {
     '/create/category': {
-	methods: ['post'],
-	middleware: [auth.ensureAuthenticated],
+	methods: ['create'],
+	middleware: [],
 	fn: function(req, res, next) {
-	    let category = Categories.findCategory(name);
-	    if(category instanceof Error) {
-		Category.newCategory({ name: req.body.name }, function(err) {
-		if(err) return next(err);
-		    res.json({ result: "Success" });
-		});
-		    
-	    }
+	     const name = req.body.name;
+	     Category.findOne({name: name}, function (err, result) {
+		     if (err) {
+			 console.log('Error!!!');
+		     } else if (result) {
+			 console.log('Already exists!!11!');
+		     } else {
+			 let cat = new Category({name: name});
+			 cat.save(function (err) {
+			      if (err) {
+				  return console.log("err");
+			      } else {
+				  res.json({result: "YES!!!!"});
+			      }
+			 });
+		     }
+	     });
 	}
     }
 }
