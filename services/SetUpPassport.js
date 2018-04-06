607f5fdfd5a9c3f0da7446a583f3873d5ffaeb61
Tyler Cook <tylercook017@gmail.com>
2018-04-06 12:49:32 -0700

    Passport - Fixed field

--- services/SetUpPassport.js
+++ services/SetUpPassport.js
@@ -6,7 +6,7 @@ module.exports = function() {
   passport.serializeUser(function(mod, done) {
     done(null, mod._id);
   });
-    
+
   passport.deserializeUser(function(_id, done) {
     Mod.findById(_id, function(err, mod) {
       done(err, mod);
@@ -14,14 +14,14 @@ module.exports = function() {
   });
 }
 
-let strategy = new LocalStrategy(function(email, password, done) {
+let strategy = new LocalStrategy({ usernameField: 'email' }, function(email, password, done) {
     Mod.findOne({ email: email }, function(err, mod) {
     if(err) {
       return done(err);
     } else if(!mod) {
       return done(null, false, { message: "No such mod of email " + email });
     }
-	
+
     mod.checkPassword(password, function(err, matching) {
       if(err) {
         return done(err);
