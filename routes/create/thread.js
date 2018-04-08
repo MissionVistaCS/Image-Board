f3014a73fdbcd7da7f881ac0bf07d8c3d0132bb1
Andrew Yates <andrewmacu@me.com>
2018-04-07 23:19:23 -0700

    rtune or to take arms against a sea of troubles and by opposing end them

--- routes/create/thread.js
+++ routes/create/thread.js
@@ -2,70 +2,89 @@ const Thread = require(_base + 'models/thread');
 const fs = require('fs');
 const path = 'uploads/';
 const multer = require('multer');
-const upload = multer({ dest: './uploads/'});
+const upload = multer({dest: './uploads/'});
 
 module.exports = {
-	'/create/thread' : {
-	    methods: ['post'],
-	    middleware: [upload.single("attachment")],
-	        fn: function (req, res, next) {
-		    console.log(req.file);
-			let name = req.body.name,
-			    board = req.body.board,
-			    attachment = req.file,
-			    pinned = req.body.pinned,
-			    ip = req.connection.remoteAddress,
-			    content = req.body.content,
-			    title = req.body.title;
+    '/create/thread': {
+        methods: ['post'],
+        middleware: [upload.single("attachment")],
+        fn: function (req, res, next) {
+            console.log(req.file);
+            let name = req.body.name,
+                board = req.body.board,
+                attachment = req.file,
+                pinned = req.body.pinned,
+                ip = req.connection.remoteAddress,
+                content = req.body.content,
+                title = req.body.title;
 
-			Thread.findOne({ title: title }, function (err, result) {
-				if (err) {
-					return next(err);
-				}
+            Thread.findOne({title: title}, function (err, result) {
+                if (err) {
+                    return next(err);
+                }
 
-				if (result) {
-					return next(new Error('Thread with that title already exists.'));
-				}
-				let thread = new Thread({ name: name, boardId: board, pinned: pinned, ip: ip, content: content, title: title });
+                if (result) {
+                    return next(new Error('Thread with that title already exists.'));
+                }
+                let thread = new Thread({
+                    name: name,
+                    boardId: board,
+                    pinned: pinned,
+                    ip: ip,
+                    content: content,
+                    title: title
+                });
 
-				if (attachment) {
-					let target_path = path + attachment.filename + "." + attachment.originalname.split('.').pop();
-					thread.attachment_path = target_path;
-					thread.attachment_name = attachment.originalname;
-				}
+                if (attachment) {
+                    let target_path = path + attachment.filename + "." + attachment.originalname.split('.').pop();
+                    thread.attachment_path = target_path;
+                    thread.attachment_name = attachment.originalname;
+                }
 
-				thread.save(function(err) {
-				    console.log(req.files);
-				        if(err) {
-						return next(err);
-					}
+                thread.save(function (err) {
+                    console.log(req.files);
+                    if (err) {
+                        return next(err);
+                    }
 
-				        if(attachment) {
-				            //Save file to fs	
-				            fs.rename(attachment.path, target_path, function(err) {
-					        if(err) {
-					            return next(err);
-					        }
-					
-				     	        fs.unlink(attachment.path, function() {
-					            if(err) {
-						        return next(err);
-					            }
-					        });
-					    });
-				        }
-				    
-					res.json({ result: { 
-						name: name, 
-						board: board,
-   					        attachment_name: attachment.originalname,
-					        content: content,
-					        title: title,
-					        pinned: pinned
-					}
-					});
-				});
-			});
-		}
-	}
-}
+                    if (attachment) {
+                        //Save file to fs
+                        fs.rename(attachment.path, target_path, function (err) {
+                            if (err) {
+                                return next(err);
+                            }
+
+                            fs.unlink(attachment.path, function () {
+                                if (err) {
+                                    return next(err);
+                                }
+
+                                res.json({
+                                    result: {
+                                        name: name,
+                                        board: board,
+                                        attachment_name: attachment.originalname,
+                                        attachment_path: target_path,
+                                        content: content,
+                                        title: title,
+                                        pinned: pinned
+                                    }
+                                });
+                            });
+                        });
+                    } else {
+                        res.json({
+                            result: {
+                                name: name,
+                                board: board,
+                                content: content,
+                                title: title,
+                                pinned: pinned
+                            }
+                        });
+                    }
+                });
+            });
+        }
+    }
+};
